6a96157644ca6dfb5b20072374785d31f12a1b3a
Ruslan Magana Vsevolodovna <Ruslan.Idelfonso.Magana.Vsevolodovna-CIC@ibm.com>
2025-07-16 18:37:02 +0200

    Update install_watsonx_pc.sh

--- scripts/ubuntu/install_watsonx_pc.sh
+++ scripts/ubuntu/install_watsonx_pc.sh
@@ -71,7 +71,7 @@ if ! docker compose version 2>/dev/null | grep -q 'v2\.'; then
 fi
 
 # Config
-ADK_VERSIONS=( "1.5.0" "1.5.1" "1.6.0" "1.6.1" "1.6.2" )
+ADK_VERSIONS=( "1.5.0" "1.5.1" "1.6.0" "1.6.1" "1.6.2" "1.7.0" )
 ENV_FILE="${INSTALL_ROOT}/.env"
 VENV_DIR="${INSTALL_ROOT}/venv"
 ADK_VERSION=""
